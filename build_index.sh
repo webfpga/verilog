e3af61f5e2e5166d5d46878690b0044978f00922
Ryan Jacobs <ryan@rmj.us>
2019-07-19 11:21:28 -0400

    wip build index

--- build_index.sh
+++ build_index.sh
@@ -4,9 +4,10 @@
 
 print_file() {
     x="$1"
-    [ "${x:0:2}" == "./" ] && x="${x:2}" # remove leading "./" if needed
-    url="https://raw.githubusercontent.com/webfpga/verilog/master/examples/$x"
-    echo "file $x $url"
+    #[ "${x:0:2}" == "./" ] && x="${x:2}" # remove leading "./" if needed
+    #url="https://raw.githubusercontent.com/webfpga/verilog/master/examples/$x"
+    #echo "file $x $url"
+    echo "$x"
 }
 
 traverse() {
@@ -15,17 +16,13 @@ traverse() {
     for x in "$root"/*; do
         if [ -d "$x" ]; then
             traverse "$x"
-            echo
         elif [ -f "$x" ]; then
             print_file "$x"
         fi
     done
 }
 
-cd examples
-traverse .
-exit
-
-for example_path in v; do
-    echo file $filename $url
-done
+echo git_commit: `git rev-parse HEAD`
+echo git_tag: `git rev-parse HEAD`
+echo; traverse ./examples
+echo; traverse ./library
